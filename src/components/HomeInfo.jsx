8016e4c1102d2b7cc4ee367f1fb0725dbe888afa
Aiturgan <aiturgan888@gmail.com>
2024-12-29 10:01:01 -0600

    fixing the flow

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,7 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import headshot from '../assets/images/hs1.png';
-
+import mic from '../assets/images/mic.PNG';
 const InfoBox = ({ text, link, btntext }) => (
 	<div className="sm:text-xl rounded-lg flex items-center shadow-md  sm:leading-snug text-center text-violet-900 py-4 px-8 bg-white ">
 		<p className=" w-42 sm:text-xl  text-violet-900 font-meduim"> {text}</p>
@@ -17,11 +17,25 @@ const renderContent = {
 	1: (
 		<div className="sm:text-xl rounded-lg flex items-center shadow-md  sm:leading-snug text-center text-violet-900 py-4 px-8 bg-white   >">
 			<img src={headshot} alt="Headshot" className="w-32 h-32 "></img>
-			<h1 className="w-42 sm:text-xl  text-violet-900 font-meduim">Hey! I'm Aiturgan, software engineer and creative thinker!</h1>
+			<h1 className="w-42 sm:text-xl  text-violet-900 font-meduim">
+				Hey! I'm Aiturgan Talant, welcome to my portfolio! Press on the screen to learn more about what I do!
+			</h1>
 		</div>
 	),
-	2: <InfoBox text="I have interned in Microsoft and have extensive technical and communication skills!" link="/about" btntext="Learn more!"></InfoBox>,
-	3: <InfoBox text="I have built several hackathon winning projects. Moreover, I do art too!" link="/projects" btntext="Learn more!"></InfoBox>,
+	2: (
+		<InfoBox
+			text="I am software engineer with expirience at Microsoft, UX/UI design hobbyist and hackathon enthusiast!"
+			link="/about"
+			btntext="Learn more about me!"
+		></InfoBox>
+	),
+	3: (
+		<InfoBox
+			text="I enjoy fullstack developement and have built several hackathon winning projects! Moreover, I do art and design too!"
+			link="/projects"
+			btntext="See my work!"
+		></InfoBox>
+	),
 	4: <InfoBox text="If you would like to work with me, don't hestiate to reach out!" link="/contact" btntext="Contact!"></InfoBox>,
 };
 
