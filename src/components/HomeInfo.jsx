0d7a8227b927a00d8559dbcf18f4bb818b3932f4
Aiturgan <aiturgan888@gmail.com>
2024-12-29 13:09:42 -0600

    fixed styling of infoboxes

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,16 +1,20 @@
 import { FaLongArrowAltRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import contact from '../assets/images/contact.PNG';
 import headshot from '../assets/images/hs1.png';
 import mic from '../assets/images/mic.PNG';
+import work from '../assets/images/work.PNG';
 const InfoBox = ({ text, link, btntext, imgsrc, imgalt }) => (
 	<div className="sm:text-xl rounded-lg flex items-center shadow-md  sm:leading-snug text-center text-violet-900 py-4 px-8 bg-white ">
 		{imgsrc && <img src={imgsrc} alt={imgalt || 'Image'} className="w-32 h-32" />}
-		<p className=" w-42 sm:text-xl  text-violet-900 font-meduim"> {text}</p>
-		<Link to={link} className="">
-			{btntext}
-			<FaLongArrowAltRight style={{ color: 'violet' }} />
-		</Link>
+		<div className=" flex flex-col items-center gap-4">
+			<p className=" w-42 sm:text-xl  text-violet-900 font-meduim"> {text}</p>
+			<Link to={link} className="flex items-center gap-2 ">
+				{btntext}
+				<FaLongArrowAltRight style={{ color: 'violet' }} />
+			</Link>
+		</div>
 	</div>
 );
 
@@ -19,7 +23,7 @@ const renderContent = {
 		<div className="sm:text-xl rounded-lg flex items-center shadow-md  sm:leading-snug text-center text-violet-900 py-4 px-8 bg-white   >">
 			<img src={headshot} alt="Headshot" className="w-32 h-32 "></img>
 			<h1 className="w-42 sm:text-xl  text-violet-900 font-meduim">
-				Hey! I'm Aiturgan Talant, welcome to my portfolio! Press on the screen to learn more about what I do!
+				Hey! I'm Aiturgan Talant, welcome to my portfolio! Long press on the screen to learn more about what I do!
 			</h1>
 		</div>
 	),
@@ -34,12 +38,22 @@ const renderContent = {
 	),
 	3: (
 		<InfoBox
+			imgsrc={work}
+			imgalt="OpenCV project"
 			text="I enjoy fullstack developement and have built several hackathon winning projects! Moreover, I do art and design too!"
 			link="/projects"
 			btntext="See my work!"
 		></InfoBox>
 	),
-	4: <InfoBox text="If you would like to work with me, don't hestiate to reach out!" link="/contact" btntext="Contact!"></InfoBox>,
+	4: (
+		<InfoBox
+			imgsrc={contact}
+			imgalt="Working together"
+			text="If you would like to work with me, don't hestiate to reach out!"
+			link="/contact"
+			btntext="Contact!"
+		></InfoBox>
+	),
 };
 
 const HomeInfo = ({ currentStage }) => {
