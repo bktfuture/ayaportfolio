a2b958a3f3ad7db28307bdfd8283ed43302e4ea1
Aiturgan <aiturgan888@gmail.com>
2024-12-31 03:20:40 -0600

    about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,27 +4,51 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import { experiences, skills } from '../constants';
 
 import CTA from '../components/CTA';
+import mcr from '../assets/images/mc.png';
 import me from '../assets/images/me.JPEG';
+import mlh from '../assets/images/mlh.png';
+import stlcc from '../assets/images/stl.jpg';
+import uber from '../assets/images/uber.png';
+import umsl from '../assets/images/umsl.png';
 
 const About = () => {
 	return (
 		<section className="max-container">
-			<div className="flex w-full justify-between">
-				<h1 className="head-text">
-					Hello, <br></br>I'm <span className="violet-gradient_text font-semibold drop-shadow"> Aiturgan!</span> 👋
-				</h1>
+			<div className="flex w-full gap-2 justify-between">
+				<div className="flex gap-4 flex-col">
+					<h1 className="head-text">
+						Hello, <br></br>I'm <span className="violet-gradient_text font-semibold drop-shadow"> Aiturgan!</span> 👋
+					</h1>
+					<div className="flex gap-3 items-center">
+						<img src={umsl} alt="University of Missouri-St. Louis" className="w-[60px]"></img>
+						<p>Computer Science, Graduation date: 2027</p>
+					</div>
+					<div className="flex gap-3 items-center">
+						<img src={stlcc} alt="St. Louis Community College" className="w-[60px]"></img>
+						<p>"Pillar of STLCC" Award Recipient, Dean's List, 2024 President of TRIO Student Leadership Board, 2023 Women in STEM President</p>
+					</div>
+					<div className="flex gap-3 items-center">
+						<img src={uber} alt="University of Missouri-St. Louis" className="w-[60px]"></img>
+						<p>2024 UCP Software Engineer Fellow</p>
+					</div>
+					<div className="flex gap-3 items-center">
+						<img src={mcr} alt="University of Missouri-St. Louis" className="w-[60px]"></img>
+						<p>Asians @ Microsoft Mentoring International Program Recipient</p>
+					</div>
+					<div className="flex gap-3 items-center">
+						<img src={mlh} alt="University of Missouri-St. Louis" className="w-[60px]"></img>
+						<p>8 times hackathon winner with Mobile, VR and fullstack apps</p>
+					</div>
+				</div>
+
 				<img src={me} alt="Collage of photos" className="max-w-md"></img>
 			</div>
 
-			<div className="mt-5 flex  gap-3 text-slate-500">
-				<p>
-					Second Year Computer Science student at the University of Missouri-St.Louis. 6 Hackathon Winner including MIT XR Hackathon Qualcomm AR
-					glasses recipient, BobaTalks x Grow With Google Winner, and more! - "Pillar of STLCC" Award Winner and twice elected President of TRIO
-					Student Leadership Board
-				</p>
+			<div className="mt-10 flex justify-center gap-3 text-slate-500">
+				<p>🌟 Outside of academics, I enjoy everything cosmos and travel related! I love dancing, filming and hiking!</p>
 			</div>
 			<div className="py-10 flex flex-col">
-				<h3 className="subhead-text">My Skills</h3>
+				<h3 className="subhead-text">My Skills ⚙️</h3>
 
 				<div className="mt-16 flex flex-wrap gap-12">
 					{skills.map((skill) => (
@@ -38,7 +62,7 @@ const About = () => {
 				</div>
 			</div>
 			<div className="py-16">
-				<h3 className="subhead-text">Work Experience.</h3>
+				<h3 className="subhead-text">Work Experience 👩🏽‍💻</h3>
 				<div className="mt-5 flex flex-col gap-3 text-slate-500">
 					<p>I've worked with all sorts of companies, leveling up my skills and teaming up with smart people. Here's the rundown:</p>
 				</div>
