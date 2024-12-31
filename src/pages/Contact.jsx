c99ad047c55f5d414b918e721a1095a31118f441
Aiturgan <aiturgan888@gmail.com>
2024-12-30 19:48:37 -0600

    styleing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import { FaGithub } from 'react-icons/fa6';
 import { FaLinkedin } from 'react-icons/fa';
+import { IoMdDocument } from 'react-icons/io';
 import { IoMdMail } from 'react-icons/io';
 import emailjs from '@emailjs/browser';
 import together from '../assets/images/together.jpg';
@@ -54,20 +55,28 @@ const Contact = () => {
 	return (
 		<section className="relative flex lg:flex-row flex-col max-container">
 			<div className="flex-1 min-w-[50%] flex flex-col">
-				<h1 className="head-text">My contact information, resume and more!</h1>
-				<div className="flex lg:flex-row flex-col w-full justify-between">
-					<div className="m">
+				<h1 className="head-text">My contact information 💜</h1>
+				<div className="flex lg:flex-row flex-col w-full justify-between pt-7">
+					<div className="flex gap-4 flex-col">
 						<a href="aiturgan888@gmail.com" target="_blank" className="flex items-center gap-4 text-lg">
-							<IoMdMail style={{ color: 'purple', fontSize: '42px' }} />
+							<IoMdMail style={{ color: '#8f0eff', fontSize: '42px' }} />
 							aiturgan888@gmail.com
 						</a>
 						<a href="https://www.linkedin.com/in/aiturgantalant/" target="_blank" className="flex items-center gap-4 underlined">
-							<FaLinkedin style={{ color: 'purple', fontSize: '42px' }} />
+							<FaLinkedin style={{ color: '#8f0eff', fontSize: '42px' }} />
 							<span className="text-lg">linkedin.com/in/aiturgantalant/</span>
 						</a>
 						<a href="https://github.com/bktfuture" target="_blank" className="flex items-center gap-4 text-lg">
-							<FaGithub style={{ color: 'purple', fontSize: '42px' }} />
+							<FaGithub style={{ color: '#8f0eff', fontSize: '42px' }} />
 							github.com/bktfuture
+						</a>
+						<a
+							href="https://drive.google.com/file/d/13z21ByAycHrfEei7a2-gw_cF99-5dvPA/view?usp=drive_link"
+							target="_blank"
+							className="flex items-center gap-4 text-lg"
+						>
+							<IoMdDocument style={{ color: '#8f0eff', fontSize: '42px' }} />
+							resume
 						</a>
 					</div>
 					<div className="flex flex-col gap-2">
@@ -75,7 +84,7 @@ const Contact = () => {
 						<p className=" max-w-xs italic text-sm">Me and my friends at the Harvard Polkadot Hackthon with Conor Daly, IndyCar/NASCAR Driver</p>
 					</div>
 				</div>
-
+				<h1 className="head-text">Get in touch!</h1>
 				<form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
 					<label className="text-black-500 font-semibold">
 						Name
