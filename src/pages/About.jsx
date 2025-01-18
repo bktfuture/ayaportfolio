81f01b5a16b3b4108f4d4bf3d977e594bf1b88b2
Aiturgan <aiturgan888@gmail.com>
2025-01-17 20:04:46 -0600

    changes flow for about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,19 +47,6 @@ const About = () => {
 			<div className="mt-10 flex justify-center gap-3 text-slate-500">
 				<p>🌟 Outside of academics, I enjoy everything cosmos and travel related! I love dancing, filming and hiking!</p>
 			</div>
-			<div className="py-10 flex flex-col">
-				<h3 className="subhead-text">My Skills ⚙️</h3>
-				<div className="mt-16 flex flex-wrap gap-12">
-					{skills.map((skill) => (
-						<div className="block-container w-20 h-20" key={skill.name}>
-							<div className="btn-back rounded-xl" />
-							<div className="btn-front rounded-xl flex justify-center items-center">
-								<img src={skill.imageUrl} alt={skill.name} className="w-1/2 h-1/2 object-contain" />
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
 			<div className="py-16">
 				<h3 className="subhead-text">Work Experience 👩🏽‍💻</h3>
 				<div className="mt-5 flex flex-col gap-3 text-slate-500">
@@ -120,6 +107,20 @@ const About = () => {
 					</VerticalTimeline>
 				</div>
 			</div>
+			<div className="py-10 flex flex-col">
+				<h3 className="subhead-text">My Skills ⚙️</h3>
+				<div className="mt-16 flex flex-wrap gap-12">
+					{skills.map((skill) => (
+						<div className="block-container w-20 h-20" key={skill.name}>
+							<div className="btn-back rounded-xl" />
+							<div className="btn-front rounded-xl flex justify-center items-center">
+								<img src={skill.imageUrl} alt={skill.name} className="w-1/2 h-1/2 object-contain" />
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+
 			<hr className="border-violet-600" />
 			<CTA />
 		</section>
